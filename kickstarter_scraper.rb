2eae5e0a05f5071db64e2188ae3a08970bf66c26
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-29 01:32:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,6 +4,11 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  
+  projects = {}
+  
+  
+  
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
