31730ff50c44f2260adb3cffbf0e204934e8371c
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-29 00:05:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,6 +4,7 @@ require "pry"
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  # projects: kickstarter.css("li.project.grid_4")
   binding.pry
 end
  
